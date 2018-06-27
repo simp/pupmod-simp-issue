f475f141a4d765ad7d918ef242fe3f4faeee356d
Trevor Vaughan <tvaughan@onyxpoint.com>
2018-06-27 15:13:24 -0400

    (SIMP-4680) Migrate to simp_banners
    Moved the banner information out of the module and into a separate
    `simp_banners` module so that they can both be used by other modules as
    well as being moved ahead without requiring additional version bumps to
    unrelated modules.
    
    SIMP-4738 #close

--- spec/classes/issue_spec.rb
+++ spec/classes/issue_spec.rb
@@ -1,90 +1,83 @@
 require 'spec_helper'
 
 describe 'issue', type: :class do
-  describe 'On Linux' do
-    let(:facts) do
-      {
-        kernel: 'Linux',
-        operatingsystem: 'TestOS',
-        osfamily: 'Debian',
-        memoryfree: '1 KB',
-        domain: 'testdomain'
-      }
-    end
-    context 'When nothing is specified' do
-      it do
-        should contain_File('/etc/issue').with(
-          ensure: 'file',
-          content: /ATTENTION/
-        )
-        should contain_File('/etc/issue.net').with(
-          ensure: 'file',
-          source: 'file:///etc/issue'
-        )
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      context 'When nothing is specified' do
+        it do
+          should contain_File('/etc/issue').with(
+            ensure: 'file',
+            content: /ATTENTION/
+          )
+          should contain_File('/etc/issue.net').with(
+            ensure: 'file',
+            source: 'file:///etc/issue'
+          )
+        end
       end
-    end
 
-    context 'When content is specified' do
-      let(:params) do
-        {
-          content: 'Hello!'
-        }
+      context 'When content is specified' do
+        let(:params) do
+          {
+            content: 'Hello!'
+          }
+        end
+        it do
+          should contain_File('/etc/issue').with(
+            ensure: 'file',
+            content: 'Hello!'
+          )
+        end
       end
-      it do
-        should contain_File('/etc/issue').with(
-          ensure: 'file',
-          content: 'Hello!'
-        )
-      end
-    end
 
-    context 'When net_link is true' do
-      let(:params) do
-        {
-          net_link: true
-        }
+      context 'When net_link is true' do
+        let(:params) do
+          {
+            net_link: true
+          }
+        end
+        it do
+          should contain_File('/etc/issue').with(
+            ensure: 'file',
+            content: /ATTENTION/
+          )
+          should contain_File('/etc/issue.net').with(
+            ensure: 'file',
+            source: 'file:///etc/issue'
+          )
+        end
       end
-      it do
-        should contain_File('/etc/issue').with(
-          ensure: 'file',
-          content: /ATTENTION/
-        )
-        should contain_File('/etc/issue.net').with(
-          ensure: 'file',
-          source: 'file:///etc/issue'
-        )
-      end
-    end
 
-    context 'When net_link is false and net_content is specified' do
-      let(:params) do
-        {
-          net_link: false,
-          net_content: 'Hello!'
-        }
+      context 'When net_link is false and net_content is specified' do
+        let(:params) do
+          {
+            net_link: false,
+            net_content: 'Hello!'
+          }
+        end
+        it do
+          should contain_File('/etc/issue').with(
+            ensure: 'file',
+            content: /ATTENTION/
+          )
+          should contain_File('/etc/issue.net').with(
+            ensure: 'file',
+            content: 'Hello!'
+          )
+        end
       end
-      it do
-        should contain_File('/etc/issue').with(
-          ensure: 'file',
-          content: /ATTENTION/
-        )
-        should contain_File('/etc/issue.net').with(
-          ensure: 'file',
-          content: 'Hello!'
-        )
-      end
-    end
 
-    context 'When net_link is false and net_content is not specified' do
-      let(:params) do
-        {
-          net_link: false
-        }
+      context 'When net_link is false and net_content is not specified' do
+        let(:params) do
+          {
+            net_link: false
+          }
+        end
+        it do
+          expect{ is_expected.to compile }.to raise_error(/needs to be provided/)
+        end
       end
-      it do
-        expect{ is_expected.to compile }.to raise_error(/needs to be provided/)
-      end
-    end
 
+    end
   end
 end
